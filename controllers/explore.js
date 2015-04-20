6299de30e8722d3e26b906f89e0ff317abc8d95e
sumanthv2 <sumanthferrari@gmail.com>
2015-04-20 08:47:01 -0700

    Fixing search functionality of jobs based on state

--- controllers/explore.js
+++ controllers/explore.js
@@ -2,12 +2,14 @@ var User = require('../models/User');
 var Job = require('../models/Job');
 
 exports.explore = function(req, res) {
-	state = req.params('state');
-	stateName = state.replace(/%20/g, " ");
-	if (stateName == 'undefined') {
+	state = req.query.state;
+	if (undefined != state) {
+		stateName = state.replace(/%20/g, " ");
+	} else {
 		stateName = "";
 	}
-	Job.find({ location : '/^stateName$/i'}, function (err, joblist) {
+
+	Job.find({ location : new RegExp(stateName)}, function (err, joblist) {
 		res.render('explore', {
 			title : 'Find a job',
 			jobs : joblist
